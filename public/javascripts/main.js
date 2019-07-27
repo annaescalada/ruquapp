1fb543a48aa512b8ba7e1e3b14d97b2d3b995823
annaescalada <annaescalada@gmail.com>
2019-07-27 12:21:50 +0200

    Profile auth done

---
+++ public/javascripts/main.js
@@ -0,0 +1,50 @@
+'use strict';
+const main = () => {
+  // Profile page
+  function editInfo () {
+    const editInfoButton = document.querySelector('.edit-info');
+    editInfoButton.addEventListener('click', event => {
+      const inputsInfo = document.querySelectorAll('.edit-info-input');
+      inputsInfo.forEach(input => {
+        input.removeAttribute('disabled');
+      });
+      const button = document.createElement('button');
+      button.setAttribute('type', 'submit');
+      button.innerHTML = 'Save changes';
+      // const mailInput = document.querySelector('#email');
+      // mailInput.innerHTML = mailInput.value;
+      const formInfo = document.querySelector('.edit-info-form');
+      formInfo.appendChild(button);
+    });
+  }
+
+  function editPassword () {
+    const editPasswordButton = document.querySelector('.edit-password');
+    editPasswordButton.addEventListener('click', event => {
+      const inputPassword = document.querySelector('.edit-password-input');
+      inputPassword.removeAttribute('disabled');
+      inputPassword.removeAttribute('value');
+      const button = document.createElement('button');
+      button.setAttribute('type', 'submit');
+      button.innerHTML = 'Save changes';
+      const formPassword = document.querySelector('.edit-password-form');
+      formPassword.appendChild(button);
+    });
+  }
+  // Dashboard page
+
+  // Delete pet and found
+  // Lost found toggle
+
+  // Matches page
+  // notification on and off
+  // delete match
+  // send information
+  // list map toggle
+  // map
+
+  editInfo();
+  editPassword();
+};
+
+window.addEventListener('load', main);
--- routes/profile.js
+++ routes/profile.js
@@ -3,14 +3,51 @@
 const express = require('express');
 const router = express.Router();
 
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+const User = require('../models/User');
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('profile');
+  const user = req.session.currentUser;
+  res.render('profile', user);
 });
 
 router.post('/logout', (req, res, next) => {
   delete req.session.currentUser;
   res.redirect('/');
+});
+
+router.post('/edit', async (req, res, next) => {
+  try {
+    const { name, surname, phone } = req.body;
+    const currentUserID = req.session.currentUser._id;
+    await User.findByIdAndUpdate(currentUserID, { name, surname, phone });
+    const newUser = await User.findById(currentUserID);
+    req.session.currentUser = newUser;
+    res.redirect('/profile');
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post('/edit-password', async (req, res, next) => {
+  try {
+    const { password } = req.body;
+    const currentUserID = req.session.currentUser._id;
+
+    const salt = bcrypt.genSaltSync(saltRounds);
+    const hashedPassword = bcrypt.hashSync(password, salt);
+
+    await User.findByIdAndUpdate(currentUserID, { password: hashedPassword });
+
+    const newUser = await User.findById(currentUserID);
+    req.session.currentUser = newUser;
+
+    res.redirect('/profile');
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
