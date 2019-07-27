851769e33bd5b0d0770ef4fc1ee15d8146d1a7d7
Esther Bernal <esther.bernal.lopez@gmail.com>
2019-07-27 15:16:57 +0200

    form add pet finished

--- routes/pet.js
+++ routes/pet.js
@@ -10,7 +10,7 @@ router.get('/add', isNotLoggedIn, (req, res, next) => {
   res.render('addPet');
 });
 
-router.post('/add', isNotLoggedIn (req, res, next) => {
+router.post('/add', isNotLoggedIn, (req, res, next) => {
   //
 });
 
