8bcbe4b3c26f6c2734ecbc8dbd6168b0acac9fa1
annaescalada <annaescalada@gmail.com>
2019-07-31 15:26:21 +0200

    Validate ID

--- routes/pet.js
+++ routes/pet.js
@@ -12,6 +12,7 @@ const Match = require('../models/Match');
 const parser = require('../config/cloudinary');
 
 var mongoose = require('mongoose');
+const ObjectId = mongoose.Types.ObjectId;
 
 router.get('/add', isNotLoggedIn, (req, res, next) => {
   const data = {
@@ -176,95 +177,126 @@ router.post('/add', parser.single('photo'), isNotLoggedIn, isAddPetFormFilled, a
 });
 
 router.get('/:dogID/matches', isNotLoggedIn, async (req, res, next) => {
-  const { dogID } = req.params;
-  const dog = await Dog.findById(dogID);
-  const status = dog.status;
-  const idLostDog = mongoose.Types.ObjectId(dogID);
-  const idFoundDog = mongoose.Types.ObjectId(dogID);
-  let matchesCurrentDog = [];
-  // let lost = false;
-  // let found = false;
+  try {
+    const { dogID } = req.params;
+    if (!ObjectId.isValid(dogID)) {
+      next();
+    }
+    const dog = await Dog.findById(dogID);
+    if (!dog) {
+      next();
+    }
+    const status = dog.status;
+    const idLostDog = mongoose.Types.ObjectId(dogID);
+    const idFoundDog = mongoose.Types.ObjectId(dogID);
+    let matchesCurrentDog = [];
+    // let lost = false;
+    // let found = false;
 
-  if (status === 'lost') {
-    matchesCurrentDog = await Match.find({ $or: [{ idLostDog }, { idFoundDog }] }).populate('idFoundDog');
+    if (status === 'lost') {
+      matchesCurrentDog = await Match.find({ $or: [{ idLostDog }, { idFoundDog }] }).populate('idFoundDog');
 
-    matchesCurrentDog.forEach(async (match) => {
-      await Match.findByIdAndUpdate(match._id, { messageRead: true, new: false });
-    });
-    console.log(matchesCurrentDog);
-    // matchesCurrentDog.forEach(match => {
-    //   lost = true;
-    //   match.lost = lost;
-    // });
-  } else {
-    matchesCurrentDog = await Match.find({ $or: [{ idLostDog }, { idFoundDog }] }).populate({
-      path: 'idLostDog',
-      populate: {
-        path: 'userID',
-        model: 'User'
-      }
-    });
-    console.log(matchesCurrentDog);
-    // matchesCurrentDog.forEach(match => {
-    //   found = true;
-    //   match.found = found;
-    // });
+      matchesCurrentDog.forEach(async (match) => {
+        await Match.findByIdAndUpdate(match._id, { messageRead: true, new: false });
+      });
+      console.log(matchesCurrentDog);
+      // matchesCurrentDog.forEach(match => {
+      //   lost = true;
+      //   match.lost = lost;
+      // });
+    } else {
+      matchesCurrentDog = await Match.find({ $or: [{ idLostDog }, { idFoundDog }] }).populate({
+        path: 'idLostDog',
+        populate: {
+          path: 'userID',
+          model: 'User'
+        }
+      });
+      console.log(matchesCurrentDog);
+      // matchesCurrentDog.forEach(match => {
+      //   found = true;
+      //   match.found = found;
+      // });
+    }
+    const data = {
+      dog,
+      matches: matchesCurrentDog
+    };
+    console.log(dog);
+    res.render('matches', data);
+  } catch (error) {
+    next(error);
   }
-  const data = {
-    dog,
-    matches: matchesCurrentDog
-  };
-  console.log(dog);
-  res.render('matches', data);
 });
 
 router.get('/:dogID/edit', isNotLoggedIn, async (req, res, next) => {
-  let dataEdit = {};
-  const { dogID } = req.params;
-  const dog = await Dog.findById(dogID);
-  let status;
-  if (dog.status === 'found') {
-    status = dog.status;
+  try {
+    let dataEdit = {};
+    const { dogID } = req.params;
+    if (!ObjectId.isValid(dogID)) {
+      next();
+    }
+    const dog = await Dog.findById(dogID);
+    if (!dog) {
+      next();
+    }
+    if (!dog) {
+      return res.redirect('/404');
+    }
+    let status;
+    if (dog.status === 'found') {
+      status = dog.status;
+    }
+    dataEdit = {
+      missingFields: req.flash('missingFieldsEdit'),
+      id: dogID,
+      status,
+      day: dog.day,
+      month: dog.month,
+      year: dog.year,
+      hour: dog.hour,
+      location: dog.location,
+      name: dog.name,
+      colorUnknown: dog.color.unknown,
+      sizeUnknown: dog.size.unknown,
+      earsUnknown: dog.ears.unknown,
+      tailUnknown: dog.tail.unknown,
+      hairUnknown: dog.hair.unknown,
+      white: dog.color.white,
+      grey: dog.color.grey,
+      black: dog.color.black,
+      darkBrown: dog.color.darkBrown,
+      lightBrown: dog.color.lightBrown,
+      red: dog.color.red,
+      small: dog.size.small,
+      medium: dog.size.medium,
+      large: dog.size.large,
+      breed: dog.breed,
+      up: dog.ears.up,
+      down: dog.ears.down,
+      longTail: dog.tail.longTail,
+      longHairy: dog.tail.longHairy,
+      shortTail: dog.tail.shortTail,
+      long: dog.hair.long,
+      short: dog.hair.short
+    };
+    // console.log(dataEdit);
+    res.render('editPet', dataEdit);
+  } catch (error) {
+    next(error);
   }
-  dataEdit = {
-    missingFields: req.flash('missingFieldsEdit'),
-    id: dogID,
-    status,
-    day: dog.day,
-    month: dog.month,
-    year: dog.year,
-    hour: dog.hour,
-    location: dog.location,
-    name: dog.name,
-    colorUnknown: dog.color.unknown,
-    sizeUnknown: dog.size.unknown,
-    earsUnknown: dog.ears.unknown,
-    tailUnknown: dog.tail.unknown,
-    hairUnknown: dog.hair.unknown,
-    white: dog.color.white,
-    grey: dog.color.grey,
-    black: dog.color.black,
-    darkBrown: dog.color.darkBrown,
-    lightBrown: dog.color.lightBrown,
-    red: dog.color.red,
-    small: dog.size.small,
-    medium: dog.size.medium,
-    large: dog.size.large,
-    breed: dog.breed,
-    up: dog.ears.up,
-    down: dog.ears.down,
-    longTail: dog.tail.longTail,
-    longHairy: dog.tail.longHairy,
-    shortTail: dog.tail.shortTail,
-    long: dog.hair.long,
-    short: dog.hair.short
-  };
-  // console.log(dataEdit);
-  res.render('editPet', dataEdit);
 });
 
 router.post('/:dogID/edit', isNotLoggedIn, isEditPetFormFilled, async (req, res, next) => {
   try {
+    const { dogID } = req.params;
+    if (!ObjectId.isValid(dogID)) {
+      next();
+    }
+    const dog = await Dog.findById(dogID);
+    if (!dog) {
+      next();
+    }
     const { status, day, month, year, hour, location, name, white, grey, black, darkBrown, lightBrown, red, size, breed, ears, tail, hair } = req.body;
     const currentDogId = req.params.dogID;
     const sizeObj = {};
@@ -381,43 +413,88 @@ router.post('/:dogID/edit', isNotLoggedIn, isEditPetFormFilled, async (req, res,
 });
 
 router.post('/:dogID/notification', isNotLoggedIn, async (req, res, next) => {
-  const { dogID } = req.params;
-  const dog = await Dog.findById(dogID);
-  if (dog.notification) {
-    await Dog.findByIdAndUpdate(dogID, { notification: false });
-  } else {
-    await Dog.findByIdAndUpdate(dogID, { notification: true });
+  try {
+    const { dogID } = req.params;
+    if (!ObjectId.isValid(dogID)) {
+      next();
+    }
+    const dog = await Dog.findById(dogID);
+    if (!dog) {
+      next();
+    }
+    if (dog.notification) {
+      await Dog.findByIdAndUpdate(dogID, { notification: false });
+    } else {
+      await Dog.findByIdAndUpdate(dogID, { notification: true });
+    }
+    res.json({ message: 'Notification updated' });
+  } catch (error) {
+    next(error);
   }
-  res.json({ message: 'Notification updated' });
 });
 
 router.post('/:dogID/delete', isNotLoggedIn, async (req, res, next) => {
-  const { dogID } = req.params;
-  await Dog.findByIdAndDelete(dogID);
-  res.json({ message: 'Dog deleted' });
+  try {
+    const { dogID } = req.params;
+    if (!ObjectId.isValid(dogID)) {
+      next();
+    }
+    const dog = await Dog.findById(dogID);
+    if (!dog) {
+      next();
+    }
+    await Dog.findByIdAndDelete(dogID);
+    res.json({ message: 'Dog deleted' });
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.post('/matches/:matchID/delete', isNotLoggedIn, async (req, res, next) => {
-  const { matchID } = req.params;
-  await Match.findByIdAndDelete(matchID);
-  res.json({ message: 'Match deleted' });
+  try {
+    const { matchID } = req.params;
+    if (!ObjectId.isValid(matchID)) {
+      next();
+    }
+    const match = await Match.findById(matchID);
+    if (!match) {
+      next();
+    }
+    await Match.findByIdAndDelete(matchID);
+    res.json({ message: 'Match deleted' });
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.post('/matches/:matchID/message', isNotLoggedIn, async (req, res, next) => {
-  const { matchID } = req.params;
-  await Match.findByIdAndUpdate(matchID, { message: true, messageRead: false });
-  const matchInfo = await Match.findById(matchID).populate({
-    path: 'idFoundDog',
-    populate: {
-      path: 'userID',
-      model: 'User'
+  try {
+    const { matchID } = req.params;
+    if (!ObjectId.isValid(matchID)) {
+      next();
     }
-  });
-  const lostDogInfo = await Match.findById(matchID).populate('idLostDog');
-  if (matchInfo.idFoundDog.notification) {
-    sendContactMail(matchInfo.idFoundDog.userID.email, matchInfo.idFoundDog.userID.name, lostDogInfo.idLostDog.name);
+    const match = await Match.findById(matchID);
+    if (!match) {
+      next();
+    }
+    await Match.findById(matchID, { message: true, messageRead: false });
+    await Match.findByIdAndDelete(matchID);
+    await Match.findByIdAndUpdate(matchID, { message: true, messageRead: false });
+    const matchInfo = await Match.findById(matchID).populate({
+      path: 'idFoundDog',
+      populate: {
+        path: 'userID',
+        model: 'User'
+      }
+    });
+    const lostDogInfo = await Match.findById(matchID).populate('idLostDog');
+    if (matchInfo.idFoundDog.notification) {
+      sendContactMail(matchInfo.idFoundDog.userID.email, matchInfo.idFoundDog.userID.name, lostDogInfo.idLostDog.name);
+    }
+    res.json({ message: 'Owner contact info sent' });
+  } catch (error) {
+    next(error);
   }
-  res.json({ message: 'Owner contact info sent' });
 });
 
 module.exports = router;
